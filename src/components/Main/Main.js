3c7c46a67076edd106b6bbd24019bd51e6c0c5ed
Lovchev Sergey <pagemaster@yandex.ru>
2022-05-09 23:33:21 +0300

    добавлена фильтрация по датам

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,9 +9,9 @@ import mainApi from "../../ulits/MainApi";
 
 function Main(props) {
   const [cards, setCards] = React.useState([]);
-  const [findedCard, setFindedCard] = React.useState('');
   const [isSearching, setIsSearching] = React.useState(false);
   const [filteredCards, setFilteredCards] = React.useState([]);
+  const [findedCard, setFindedCard] = React.useState('');
 
   React.useEffect(() => {
     mainApi.getInitialCards()
@@ -21,12 +21,12 @@ function Main(props) {
       .catch((err) => console.log(err));
   }, []);
 
-  function handleSearch(findedCard) {
+  function handleSearchByAuthor(findedCard) {
     setFindedCard(findedCard);
     setIsSearching(true)
   }
 
-  function filterCards(cards, findedCard) {
+  function filterCardsByAuthor(cards, findedCard) {
     let cardsToFilter = cards;
     let result;
 
@@ -35,22 +35,48 @@ function Main(props) {
   }
 
   React.useEffect(() => {
-      const searchResults = filterCards(cards, findedCard);
-      setFilteredCards(searchResults);
+    const searchResults = filterCardsByAuthor(cards, findedCard);
+    setFilteredCards(searchResults);
   }, [cards, findedCard]);
+
+  const [findedFirstDate, setFindedFirstDate] = React.useState('');
+  const [findedLastDate, setFindedLastDate] = React.useState('');
+  const [filteredCardsByDate, setFilteredCardsByDate] = React.useState([]);
+
+  function filterCardsByDate(cards, findedFirstDate, findedLastDate) {
+    let cardsToFilter = cards;
+    let result;
+    let firstDate = new Date(findedFirstDate);
+    let lastDate = new Date(findedLastDate);
+
+    result = cardsToFilter.filter(card => new Date(card.publishedAt.slice(0, 16)) >= firstDate && new Date(card.publishedAt.slice(0, 16)) <= lastDate)
+    return result;
+  }
+
+  React.useEffect(() => {
+    const searchResults = filterCardsByDate(cards, findedFirstDate, findedLastDate);
+    setFilteredCardsByDate(searchResults);
+  }, [cards, findedFirstDate, findedLastDate]);
+
+  function handleSearchByDate(findedFirstDate, findedLastDate) {
+    setFindedFirstDate(findedFirstDate);
+    setFindedLastDate(findedLastDate)
+    setIsSearching(true)
+  }
 
   return (
     <>
       <Header />
       <main className="main">
         <Carousel />
-        <StickyFilter cards={cards} onSearch={handleSearch} />
+        <StickyFilter cards={cards} onAuthorSearch={handleSearchByAuthor}
+          onDateSearch={handleSearchByDate} filteredCards={filteredCards} />
         {isSearching
-        ?
-        <Cards cards={filteredCards} />
-        :
-        <Cards cards={cards} />
-}
+          ? filteredCards.length > 0
+            ? <Cards cards={filteredCards} />
+            : <Cards cards={filteredCardsByDate} />
+          : <Cards cards={cards} />
+        }
       </main>
       <Footer />
     </>
--- src/components/StickyFilter/StickyFilter.js
+++ src/components/StickyFilter/StickyFilter.js
@@ -4,14 +4,24 @@ import "./StickyFilter.css";
 function StickyFilter(props) {
   const [isFilterSticky, setIsFilterSticky] = React.useState(false);
   const [isAuthorList, setIsAuthorList] = React.useState(false);
+  const [isFirstDateList, setIsFirstDateList] = React.useState(false);
+  const [isLastDateList, setIsLastDateList] = React.useState(false);
   const [isButtonContent, setIsButtonContent] = React.useState('Выберите автора');
+  const [isFirstDateButtonContent, setIsFirstDateButtonContent] = React.useState('От');
+  const [isLastDateButtonContent, setIsLastDateButtonContent] = React.useState('До');
+  const [dateError, setDateError] = React.useState('');
   const currentViewport = document.documentElement.clientWidth;
   const stickyFilterClassName = `stickyFilter ${isFilterSticky && "stickyFilter_scroll"}`;
-  const authorListClassName = `stickyFilter__author-list ${isAuthorList && "stickyFilter__author-list_opened"}`;
+  const authorListClassName = `stickyFilter__author-list ${isAuthorList && "stickyFilter__list_opened"}`;
+  const firstDateListClassName = `stickyFilter__dates-list-first ${isFirstDateList && "stickyFilter__list_opened"}`;
+  const lastDateListClassName = `stickyFilter__dates-list-last ${isLastDateList && "stickyFilter__list_opened"}`;
+
   const [findedCard, setFindedCard] = React.useState('');
+  const [findedFirstDate, setFindedFirstDate] = React.useState('');
+  const [findedLastDate, setFindedLastDate] = React.useState('');
 
   React.useEffect(() => {
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
       if (currentViewport < 1440 && this.scrollY > 336) {
         setIsFilterSticky(true)
       }
@@ -23,42 +33,93 @@ function StickyFilter(props) {
     });
   })
 
-function authorListHandler(evt) {
-  evt.preventDefault();
-  setIsAuthorList(!isAuthorList)
-}
-
-function authorCellHandler(e) {
-  setIsButtonContent(e.target.textContent)
-  setIsAuthorList(false)
-  setFindedCard(e.target.textContent);
-  return findedCard;
-}
-
-React.useEffect(() => {
-  if (findedCard) {
-    props.onSearch(findedCard)
+  function authorListHandler(evt) {
+    evt.preventDefault();
+    setIsAuthorList(!isAuthorList)
   }
-})
+
+  function firstDateListHandler(evt) {
+    evt.preventDefault();
+    setIsFirstDateList(!isFirstDateList)
+  }
+
+  function lastDateListHandler(evt) {
+    evt.preventDefault();
+    setIsLastDateList(!isLastDateList)
+  }
+
+  function authorCellHandler(e) {
+    setIsButtonContent(e.target.textContent)
+    setIsAuthorList(false)
+    setFindedCard(e.target.textContent);
+    return findedCard;
+  }
+
+  React.useEffect(() => {
+    if (findedCard) {
+      props.onAuthorSearch(findedCard)
+    }
+  })
+
+  function firstDateCellHandler(e) {
+    setIsFirstDateButtonContent(e.target.textContent)
+    setIsFirstDateList(false)
+    setFindedFirstDate(e.target.textContent);
+    return findedFirstDate;
+  }
+
+  function lastDateCellHandler(e) {
+    setIsLastDateButtonContent(e.target.textContent)
+    setIsLastDateList(false)
+    setFindedLastDate(e.target.textContent);
+    return findedLastDate;
+  }
+
+  React.useEffect(() => {
+    const firstDate = new Date(findedFirstDate);
+    const lastDate = new Date(findedLastDate);
+    const interimDate = firstDate > lastDate;
+    if (interimDate) {
+      setDateError('Первая дата должна быть меньше, или равна первой')
+    } else if (props.filteredCards.length > 0) {
+      setDateError('Произведена фильтрация по автору. Для осуществеления фильтрации по датам сбросьте фильтр по автору.')
+    } else if (findedFirstDate && findedLastDate) {
+      setDateError('Произведена фильтрация по датам')
+      props.onDateSearch(findedFirstDate, findedLastDate)
+    }
+  }, [findedFirstDate, findedLastDate, props])
 
   return (
     <div className={stickyFilterClassName}>
-      <form className="stickyFilter__form">
+      <div className="stickyFilter__content" >
         <button className="stickyFilter__author" arialabel="Open" onClick={authorListHandler}>{isButtonContent}</button>
         <div className={authorListClassName}>
           {props.cards.filter((card, index, self) => self.findIndex(
-    (t) => {return (t.author === card.author)}) === index).map((card) => (
-            <p className="stickyFilter__author-cell" key={card.url} onClick={authorCellHandler}>{card.author}</p>
-          ))}
+            (t) => { return (t.author === card.author) }) === index).map((card) => (
+              <p className="stickyFilter__author-cell" key={card.url} onClick={authorCellHandler}>{card.author}</p>
+            ))}
         </div>
         <div className="stickyFilter__dates">
-          <input className="stickyFilter__dates-input" placeholder="От" />
-          <span className="stickyFilter__dates-input-error" id="date-error"></span>
+          <button className="stickyFilter__dates-button" arialabel="Open"
+            onClick={firstDateListHandler}>{isFirstDateButtonContent}</button>
+          <div className={firstDateListClassName}>
+            {props.cards.filter((card, index, self) => self.findIndex(
+              (t) => { return (t.publishedAt.slice(0, 16) === card.publishedAt.slice(0, 16)) }) === index).map((card) => (
+                <p className="stickyFilter__dates-list-cell" key={card.url} onClick={firstDateCellHandler}>{card.publishedAt.slice(0, 16)}</p>
+              ))}
+          </div>
           <span className="stickyFilter__dates-span">~</span>
-          <input className="stickyFilter__dates-input" placeholder="До" />
-          <span className="stickyFilter__dates-input-error" id="date-error"></span>
+          <button className="stickyFilter__dates-button" arialabel="Open"
+            onClick={lastDateListHandler}>{isLastDateButtonContent}</button>
+          <div className={lastDateListClassName}>
+            {props.cards.filter((card, index, self) => self.findIndex(
+              (t) => { return (t.publishedAt.slice(0, 16) === card.publishedAt.slice(0, 16)) }) === index).map((card) => (
+                <p className="stickyFilter__dates-list-cell" key={card.url} onClick={lastDateCellHandler}>{card.publishedAt.slice(0, 16)}</p>
+              ))}
+          </div>
         </div>
-      </form>
+        <span className="stickyFilter__dates-error">{dateError}</span>
+      </div>
     </div>
   );
 }
